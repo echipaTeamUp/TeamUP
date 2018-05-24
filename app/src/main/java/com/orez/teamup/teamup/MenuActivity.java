51e4bf2c672ce02e6f20c9e2eae76cfb4d2ba9da
Tiberiu Iancu <tiberiu.iancu@gmail.com>
2018-05-24 20:30:59 +0300

    am adaugat fragments
    jucati-va si voi cu ele si zice-ti-mi daca reusiti ceva dar eu tot nu reusesc sa sa ii dau de cap. e problema si de la faptul ca trebuie un linearLayout in loc de constraintLayout dar nu mai am energie :(

--- app/src/main/java/com/orez/teamup/teamup/MenuActivity.java
+++ app/src/main/java/com/orez/teamup/teamup/MenuActivity.java
@@ -2,17 +2,24 @@ package com.orez.teamup.teamup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 
 public class MenuActivity extends AppCompatActivity {
-
-    Button mEsportsBtn;
-    Button mWorkBtn;
-    Button mSportsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void ChangeFragment(View view){
+        Fragment fragment = new Sports();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.hide(fragment).commit();
     }
 }
---
+++ app/src/main/java/com/orez/teamup/teamup/Sports.java
@@ -0,0 +1,19 @@
+package com.orez.teamup.teamup;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.Fragment;
+
+public class Sports extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sports, container, false);
+    }
+}
