50809ae44cdbb2e22a5a25afee9e4a5530f174bc
Alex Gherghe <alexluciangherghe@gmail.com>
2018-05-24 13:32:41 +0300

    as

--- app/src/main/java/com/orez/teamup/teamup/LoginActivity.java
+++ app/src/main/java/com/orez/teamup/teamup/LoginActivity.java
@@ -2,20 +2,25 @@ package com.orez.teamup.teamup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText mUsernameEt;
+    EditText mPasswordEt;
+    Button mLoginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    mUsernameEt=(EditText) findViewById(R.id.usernameEt);
+    mPasswordEt=(EditText) findViewById(R.id.passwordEt);
+    mLoginBtn=(Button) findViewById(R.id.loginBtn);
 
         //iote comentariu
 
 
     }
-
 
 }
