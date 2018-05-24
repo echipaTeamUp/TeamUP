0ff13ddf43b7de23db24de11a76aac39e9d8d5c5
Tiberiu Iancu <tiberiu.iancu@gmail.com>
2018-05-24 18:51:45 +0300

    basic menu activity

--- app/src/main/java/com/orez/teamup/teamup/MenuActivity.java
+++ app/src/main/java/com/orez/teamup/teamup/MenuActivity.java
@@ -2,8 +2,13 @@ package com.orez.teamup.teamup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    Button mEsportsBtn;
+    Button mWorkBtn;
+    Button mSportsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
