1986544033f1d5476f96d2633ecd4e582e48b489
Tiberiu Iancu <tiberiu.iancu@gmail.com>
2018-05-24 19:31:54 +0300

    signin duce la mainActivity

--- app/src/main/java/com/orez/teamup/teamup/LoginActivity.java
+++ app/src/main/java/com/orez/teamup/teamup/LoginActivity.java
@@ -22,22 +22,28 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    mUsernameEt=(EditText) findViewById(R.id.login_usernameEt);
-    mPasswordEt=(EditText) findViewById(R.id.login_passwordEt);
-    mLoginBtn=(Button) findViewById(R.id.loginBtn);
-    mSignupBtn=(Button) findViewById(R.id.login_signupBtn);
-    //OnClick care duce la activitatea de signup
-    mSignupBtn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent i=new Intent(LoginActivity.this,SignupActivity.class);
-            startActivity(i);
-        }
-    });
+        mUsernameEt = (EditText) findViewById(R.id.login_usernameEt);
+        mPasswordEt = (EditText) findViewById(R.id.login_passwordEt);
+        mLoginBtn = (Button) findViewById(R.id.loginBtn);
+        mSignupBtn = (Button) findViewById(R.id.login_signupBtn);
 
-        //iote comentariu
+        //OnClick care duce la activitatea de signup
+        mSignupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(i);
+            }
+        });
 
-
+        //OnClick care duce la activitatea de menu
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MenuActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
