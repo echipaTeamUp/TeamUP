4228a0c291e2120c58636726ba0e3c1f190c8b40
tiberiuiancu <31802439+tiberiuiancu@users.noreply.github.com>
2018-05-25 21:25:16 +0300

    am adaugat si slide_left anim

--- app/src/main/java/com/orez/teamup/teamup/Sports.java
+++ app/src/main/java/com/orez/teamup/teamup/Sports.java
@@ -10,4 +10,11 @@ public class Sports extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sports);
     }
+
+    // animeaza cand apesi pe back ca sa te intorci in activitatea trecuta
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+    }
 }
