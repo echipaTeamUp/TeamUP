d911d8f7a165d7a8373ce06ca225595af713c336
tiberiuiancu <31802439+tiberiuiancu@users.noreply.github.com>
2018-05-25 21:18:58 +0300

    animatii pt slide-right ca la iOS

--- app/src/main/java/com/orez/teamup/teamup/LoginActivity.java
+++ app/src/main/java/com/orez/teamup/teamup/LoginActivity.java
@@ -31,7 +31,6 @@ public class LoginActivity extends AppCompatActivity {
     Button mSignupBtn;
     private FirebaseAuth mAuth;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
