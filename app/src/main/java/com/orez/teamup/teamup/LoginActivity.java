a186d8ebe0608d6f2029e30426611d15adc32210
Alex Gherghe <alexluciangherghe@gmail.com>
2018-05-31 18:28:47 +0300

    am facut poza de profil

--- app/src/main/java/com/orez/teamup/teamup/LoginActivity.java
+++ app/src/main/java/com/orez/teamup/teamup/LoginActivity.java
@@ -22,6 +22,8 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
@@ -33,6 +35,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class LoginActivity extends Activity {
@@ -140,6 +148,7 @@ public class LoginActivity extends Activity {
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         DatabaseReference myRef = database.child("id").child(uid);
         user = new User();
+
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
--- app/src/main/java/com/orez/teamup/teamup/ProfileActivity.java
+++ app/src/main/java/com/orez/teamup/teamup/ProfileActivity.java
@@ -2,13 +2,21 @@ package com.orez.teamup.teamup;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 
 public class ProfileActivity extends Activity {
@@ -24,8 +32,24 @@ public class ProfileActivity extends Activity {
         mprofileImage=(ImageView) findViewById(R.id.profile_image);
         user=(User) getIntent().getSerializableExtra("User");
         user_nameTv=(TextView) findViewById(R.id.profile_name);
-        user_nameTv.setText(user.getFirst_name()+" "+user.getLast_name());
+        StorageReference ref=FirebaseStorage.getInstance().getReference();
+        String uid=FirebaseAuth.getInstance().getCurrentUser().getUid();
+        ref.child(uid).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                //daca exista poza
+                Glide.with(ProfileActivity.this)
+                        .load(uri)
+                        .into(mprofileImage);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // daca nu exista poza
+            }
+        });
 
+        user_nameTv.setText(user.getFirst_name()+" "+user.getLast_name());
         msignoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
