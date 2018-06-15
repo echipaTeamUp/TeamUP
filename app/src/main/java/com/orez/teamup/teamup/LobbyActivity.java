f198aff4884aee9871c108f90c32744e75748b8a
Alex Gherghe <alexluciangherghe@gmail.com>
2018-06-15 23:51:16 +0300

    uber button

--- app/src/main/java/com/orez/teamup/teamup/LobbyActivity.java
+++ app/src/main/java/com/orez/teamup/teamup/LobbyActivity.java
@@ -1,13 +1,19 @@
 package com.orez.teamup.teamup;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -28,6 +34,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.uber.sdk.android.core.UberSdk;
+import com.uber.sdk.android.rides.RideParameters;
+import com.uber.sdk.android.rides.RideRequestButton;
+import com.uber.sdk.android.rides.RideRequestButtonCallback;
+import com.uber.sdk.rides.client.ServerTokenSession;
+import com.uber.sdk.rides.client.SessionConfiguration;
+import com.uber.sdk.rides.client.UberRidesApi;
+import com.uber.sdk.rides.client.error.ApiError;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -43,6 +57,7 @@ public class LobbyActivity extends AppCompatActivity {
     EditText mInputMsg;
     Button get_directionsBtn;
     Button view_on_mapBtn;
+    RideRequestButton requestBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,10 +68,14 @@ public class LobbyActivity extends AppCompatActivity {
         lobby = (LobbySports) getIntent().getSerializableExtra("Lobby");
         get_directionsBtn = (Button) findViewById(R.id.get_directionsBtn);
         view_on_mapBtn = (Button) findViewById(R.id.view_on_mapBtn);
+        requestBtn = (RideRequestButton) findViewById(R.id.rideRequestBtn);
 
         final DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Chat").child(lobby.getId());
 
         mSendFab = (FloatingActionButton) findViewById(R.id.sendMessageFab);
+        //Pentru Uber
+        initialize_uber();
+
         mSendFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,7 +98,7 @@ public class LobbyActivity extends AppCompatActivity {
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     data.add(ds.getValue(ChatMessage.class));
                 }
-                
+
                 mListView.setAdapter(new LobbyActivity.MyListAdapter(LobbyActivity.this, R.layout.chat_message_item, data));
             }
 
@@ -156,10 +175,76 @@ public class LobbyActivity extends AppCompatActivity {
                 viewHolder.mTimeTv = (TextView) convertView.findViewById(R.id.chatTimeTv);
                 viewHolder.mTimeTv.setText(getItem(position).getTime());
                 viewHolder.mUserTv = (TextView) convertView.findViewById(R.id.chatUserTv);
-                viewHolder.mUserTv.setText(getItem(position).getMessageUser()+":");
+                viewHolder.mUserTv.setText(getItem(position).getMessageUser() + ":");
             }
 
             return convertView;
         }
+    }
+
+    void initialize_uber() {
+        LocationListener mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000,
+                300, mLocationListener);
+        SessionConfiguration config = new SessionConfiguration.Builder()
+                .setClientId("FyEMGUBPhsd3tdZpQF3S2spa1W5nYifH")
+                .setServerToken("SEN3_I3iKJCKoQfoYJF6WD2YbceHRt_rMf_zmka3")
+                .setRedirectUri("teamup.com/redirecturi")
+                .setEnvironment(SessionConfiguration.Environment.SANDBOX)
+                .build();
+        UberSdk.initialize(config);
+        RideParameters rideParams = new RideParameters.Builder()
+                               .setDropoffLocation(
+                        lobby.getLatitude(),lobby.getLongitude(), "Lobby location", lobby.getLocationName())
+                                .setPickupLocation(mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude(),
+                        mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude(),
+                        "Your location", "")
+                .build();
+        requestBtn.setRideParameters(rideParams);
+        ServerTokenSession session = new ServerTokenSession(config);
+        requestBtn.setSession(session);
+        requestBtn.loadRideInformation();
+        RideRequestButtonCallback callback = new RideRequestButtonCallback() {
+
+            @Override
+            public void onRideInformationLoaded() {
+                // react to the displayed estimates
+            }
+
+            @Override
+            public void onError(ApiError apiError) {
+                // API error details: /docs/riders/references/api#section-errors
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                // Unexpected error, very likely an IOException
+            }
+        };
+        requestBtn.setCallback(callback);
     }
 }
