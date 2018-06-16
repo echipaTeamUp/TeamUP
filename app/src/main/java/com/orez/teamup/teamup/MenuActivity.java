677d5a2a73c1375bc6f0c4d02a5a2d651e0aadfa
Tiberiu Iancu <tiberiu.iancu@gmail.com>
2018-06-16 18:56:01 +0300

    take

--- app/src/main/java/com/orez/teamup/teamup/MenuActivity.java
+++ app/src/main/java/com/orez/teamup/teamup/MenuActivity.java
@@ -5,34 +5,27 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Canvas;
-import android.graphics.ColorFilter;
-import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuActivity extends Activity {
 
     ImageButton mprofileBtn;
     ImageButton msignoutBtn;
     User user;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,17 +39,42 @@ public class MenuActivity extends Activity {
         msignoutBtn = (ImageButton) findViewById(R.id.menu_signoutBtn);
 
         checkLocationPermission();
+
+        // Daca esti deja intr-un lobby te baga in el
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        final String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Verifica daca esti intr-un lobby
+                String LobbyID = dataSnapshot.child("id").child(uid).child("Lobby").getValue(String.class);
+
+                // Daca da, te duce in lobby
+                if (LobbyID != null) {
+                    Intent i = new Intent(MenuActivity.this, LobbyActivity.class);
+                    i.putExtra("User", user);
+                    i.putExtra("Lobby", dataSnapshot.child("SportsLobby").child(LobbyID).getValue(LobbySports.class));
+                    startActivity(i);
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MenuActivity.this, databaseError.toException().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
         //Daca apesi pe profil, te duce la profil
         mprofileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MenuActivity.this, ProfileActivity.class);
                 i.putExtra("User", user);
-                i.putExtra("Req_code",1);
+                i.putExtra("Req_code", 1);
                 startActivity(i);
             }
         });
-
 
 
         msignoutBtn.setOnClickListener(new View.OnClickListener() {
@@ -74,7 +92,7 @@ public class MenuActivity extends Activity {
 
     public void loadSportsActivity(View view) {
         Intent i = new Intent(MenuActivity.this, SportsActivity.class);
-        i.putExtra("User",user);
+        i.putExtra("User", user);
         startActivity(i);
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
     }
@@ -104,7 +122,7 @@ public class MenuActivity extends Activity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==1 && (grantResults.length==0 || grantResults[0]==PackageManager.PERMISSION_DENIED))
-        {}// TODO:facem ceva sa blocam aplicatia daca nu avem permisiune de locatie
+        if (requestCode == 1 && (grantResults.length == 0 || grantResults[0] == PackageManager.PERMISSION_DENIED)) {
+        }// TODO:facem ceva sa blocam aplicatia daca nu avem permisiune de locatie
     }
 }
